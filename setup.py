f570006e8c9a2826b6c2e24af27741d25d3f6e36
axel.scheele <axel.scheele@nl.abnamro.com>
2021-12-08 21:20:14 +0100

    quick and easy way to get glue libs, set some base files for cdk v1

---
+++ setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+
+setuptools.setup(
+    name="harvest_template",
+    version="0.0.1",
+
+    description="An empty CDK Python app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    author="author",
+
+    package_dir={"": "harvest_template"},
+    packages=setuptools.find_packages(where="harvest_template"),
+
+    install_requires=[],
+
+    python_requires=">=3.7",
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+
+        "Intended Audience :: Developers",
+
+        "License :: OSI Approved :: Apache Software License",
+
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Utilities",
+
+        "Typing :: Typed",
+    ],
+)
