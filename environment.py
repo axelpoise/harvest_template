1b36dbc3140f7346b218e0533b62123f70d04566
axelpoise <axelpoise@users.noreply.github.com>
2023-01-25 12:50:03 +0100

    Update environment.py
    fix for cdk 2

--- environment.py
+++ environment.py
@@ -1,5 +1,5 @@
 
-from aws_cdk import core
+import aws_cdk as cdk
 
-def get_environment()-> core.Environment:
-    return core.Environment(region='eu-west-1', account='885617958043')
+def get_environment()-> cdk.Environment:
+    return cdk.Environment(region='eu-west-1', account='<<>>')
